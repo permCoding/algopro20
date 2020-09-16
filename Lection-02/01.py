77694781a745b9f4786f52b0f913291a2923ebd0
Андрей Беляков <ttxiom@gmail.com>
2020-09-16 14:37:58 +0500

    кратность

--- Lection-02/01.py
+++ Lection-02/01.py
@@ -1,1 +1,14 @@
-print(1212)
+import math
+
+def get_result(radius):
+    result = radius * math.pi  # формула
+    return result
+
+
+radius = float(input("Введите радуис - "))
+print(get_result(radius))
+
+
+# repo/labrab_01/task_05.py
+# float --> 1.5
+# Ctrl + n
