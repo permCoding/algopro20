68ac70ed126d1cf0c3a99dadb6dac85b301a5b4a
Андрей Беляков <ttxiom@gmail.com>
2020-11-05 11:56:50 +0500

    Create 00.py

---
+++ Lection-15/00.py
@@ -0,0 +1,15 @@
+# сначала нужно установить библиотеку requests
+# для этого в консоли наберите и запустите:
+# pip install requests
+import requests  
+
+link = 'https://pcoding.ru/csv/18.txt'
+# s = requests.get(link).text
+
+# txt = requests.get(link)
+# txt.encoding = 'utf-8'
+# s = txt.text
+
+s = requests.get(link).content.decode('utf-8')
+
+print(s)
